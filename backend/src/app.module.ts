5194b1634fcba5a7f55475c5d5c54b465b00f7dc
yash244466666 <yash.solo.000@gmail.com>
2024-09-19 03:21:40 +0600

    Add crud funtionality for employeeId in previous commit, add Restaurant resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 
 import { EmployeeModule } from './employee/employee.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 
 @Module({
-  imports: [PrismaModule, EmployeeModule, ],
+  imports: [PrismaModule, EmployeeModule, RestaurantModule, ],
 })
 export class AppModule {}
